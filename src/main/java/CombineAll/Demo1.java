8d8a6141eac049d6fd11175f8493c2453a45d6ef
Harsha <Harsha@DESKTOP-5JME961.Optilink>
2022-05-06 13:57:34 +0530

    my first repository

---
+++ src/main/java/CombineAll/Demo1.java
@@ -0,0 +1,17 @@
+package CombineAll;
+
+public class Demo1 {
+	
+	public int[] array() {
+		int [] abc = {10,320,400,50,500};
+		return abc;
+	}
+	
+	public static void main(String[] args) {
+		Demo1 obj=new Demo1();
+		obj.array();
+		
+		
+		}
+
+}
---
+++ src/main/java/CombineAll/PIMPage.java
@@ -0,0 +1,10 @@
+package CombineAll;
+
+public class PIMPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
